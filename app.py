252899d26f7db4c5089777d0a9adb858b8eebd42
Abhii <abhijeethiwalemail@gmail.com>
2023-11-15 22:06:01 +0530

    updates

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from flask_restful import Api, Resource
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgres://wanderlust_db_kv6b_user:H4TfcxmDUZShtZs9YChZsKvCJb3pntAM@dpg-cl72uv8icrhc73d0ge10-a.oregon-postgres.render.com/wanderlust_db_kv6b')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgres://wanderlust_db_kv6b_user:H4TfcxmDUZShtZs9YChZsKvCJb3pntAM@dpg-cl72uv8icrhc73d0ge10-a.oregon-postgres.render.com/wanderlust_db_kv6b')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/wanderlust_backend_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['LOG_WITH_GUNICORN'] = True
 
